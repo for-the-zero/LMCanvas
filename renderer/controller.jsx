42bdbc13d83dca46f1cfd95d77eb621d048ccc23
for-the-zero <ourmicroid@outlook.com>
2025-06-09 11:49:42 +0800

    ui(right)

--- renderer/controller.jsx
+++ renderer/controller.jsx
@@ -3,55 +3,80 @@ import './controller.css';
 
 // components
 import { 
-    tokens,
     Button, Select, Label, Input, Radio, RadioGroup, Switch, InfoLabel,
-    Title1, Tooltip, Avatar, Divider,
+    Title1, Tooltip, Text , Divider, Link,
     Card, CardHeader, CardPreview,
-    Body1, Body1Strong
+    Body1, Body1Strong,
+    Dialog, DialogTrigger, DialogSurface, DialogTitle, DialogBody, DialogContent,
 } from '@fluentui/react-components';
-import { Collapse } from '@fluentui/react-motion-components-preview';
 // icons
 import {
-    ArrowResetRegular, SaveRegular,
-    ChatMultipleMinusRegular, BotRegular, ThinkingRegular, CodeBlockRegular, ArrowCollapseAllRegular
+    ArrowResetRegular, InfoRegular, Dismiss24Regular,
+    ChatMultipleMinusRegular, BotRegular
 } from '@fluentui/react-icons';
+// markdown
+import ReactMarkdown from 'react-markdown';
+import rehypeRaw from 'rehype-raw';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { vscDarkPlus } from 'react-syntax-highlighter/dist/esm/styles/prism';
 
-function CollapseCard({title, children}){
-    const [open, set_open] = React.useState(false);
-    return (
-        <Card appearance='outline'
-            selected={open} onSelectionChange={(e,{selected})=>{set_open(selected)}}
-            style={open? {backgroundColor: tokens.colorNeutralBackground3} : {}}
+function CodeBlock({ node, inline, className, children, ...props }){
+    const match = /(\w+):/.exec(className || '');
+    
+    return !inline && match ? (
+        <SyntaxHighlighter
+            style={vscDarkPlus}
+            language={match[1]}
+            {...props}
         >
-            <CardHeader 
-                image={
-                    (title === 'Thoughts') ? <ThinkingRegular style={{fontSize: '20px'}} /> : 
-                    <CodeBlockRegular style={{fontSize: '20px'}} />
-                }
-                header={
-                    <Body1Strong>{title}</Body1Strong>
-                }
-            />
-            {/* <CardPreview className='msg-container' style={{overflow: 'scroll', maxHeight: '150px'}}>
-                <Collapse visible={open}>
-                    {children}
-                </Collapse>
-            </CardPreview> */}
-            <Collapse visible={open}>
-                <CardPreview className='msg-container' style={{overflow: 'scroll', maxHeight: '150px'}}>
-                    {children}
-                </CardPreview>
-            </Collapse>
-        </Card>
-    )
+            {String(children).replace(/\n$/, '')}
+        </SyntaxHighlighter>
+    ) : (
+        <code {...props} style={{'padding': '1px','borderRadius': '3px', 'border': '1.5px solid gray'}}>
+            {children}
+        </code>
+    );
 };
+const ThinkingBlock = ({ children }) => {
+    return (
+        <Card appearance='outline' size='small'>
+            <CardHeader
+                header={<Body1Strong>Thoughts:</Body1Strong>}
+            />
+            <CardPreview style={{padding: '8px', paddingTop: '0'}}>
+                {children}
+            </CardPreview>
+        </Card>
+    );
+};
+
+const markdown_cpnts = {
+    strong: ({ children }) => (<Text weight="bold">{children}</Text>),
+    em: ({ children }) => (<Text italic>{children}</Text>),
+    u: ({ children }) => (<Text underline>{children}</Text>),
+    del: ({ children }) => (<Text strikethrough>{children}</Text>),
+    hr: () => <Divider />,
+    a: ({ children, href }) => (<Link href={href}>{children}</Link>),
+    code: CodeBlock,
+    think: ({ children }) => (<ThinkingBlock>{children}</ThinkingBlock>),
+};
+const MsgMD = React.memo(({children})=>{
+    return (
+        <ReactMarkdown
+            rehypePlugins={[rehypeRaw]}
+            components={markdown_cpnts}
+        >
+            {children}
+        </ReactMarkdown>
+    )
+});
+
 
 function App() {
     const [useasak, set_useasak] = React.useState(false);
     const [streaming, set_streaming] = React.useState(true);
     const [asak_configusefile, set_asak_configusefile] = React.useState(true);
     const [asak_recordusefile, set_asak_recordusefile] = React.useState(true);
-
 
 
     return (
@@ -64,9 +89,40 @@ function App() {
                     <Tooltip content={'Reset'} relationship='label'>
                         <Button appearance="subtle" icon={<ArrowResetRegular />} />
                     </Tooltip>
-                    <Tooltip content={'Save'} relationship='label'>
-                        <Button appearance="subtle" icon={<SaveRegular />} />
-                    </Tooltip>
+                    <Dialog>
+                        <DialogTrigger disableButtonEnhancement>
+                            <Tooltip content={'About'} relationship='label'>
+                                <Button appearance="subtle" icon={<InfoRegular />} />
+                            </Tooltip>
+                        </DialogTrigger>
+                        <DialogSurface>
+                            <DialogBody>
+                                <DialogTitle 
+                                    action={
+                                        <DialogTrigger action="close">
+                                            <Button
+                                                appearance="subtle"
+                                                aria-label="close"
+                                                icon={<Dismiss24Regular />}
+                                            />
+                                        </DialogTrigger>
+                                    }
+                                >
+                                    About LMCanvas
+                                </DialogTitle>
+                                <DialogContent>
+                                    <MsgMD>{`
+Go to see my [Github repo](https://github.com/for-the-zero/LMcanvas)
+
+UI: [Fluent UI React Components](https://react.fluentui.dev/)
+
+TODO - Add description here 
+                                    
+                                    `}</MsgMD>
+                                </DialogContent>
+                            </DialogBody>
+                        </DialogSurface>
+                    </Dialog>
                 </div>
 
                 <Label>System prompt language:</Label>
@@ -137,39 +193,74 @@ function App() {
             </div>
 
             <div className="right">
+                <div className='chats'>
 
-                <Card appearance='subtle'>
-                    <CardHeader 
-                        image={
-                            <ChatMultipleMinusRegular style={{fontSize: '20px'}} />
-                        }
-                        header={
-                            <Body1Strong>AI received message</Body1Strong>
-                        } />
-                    <CardPreview className='msg-container'>
-                        <Body1>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nisi, ipsam sequi minima optio in vel sed dicta blanditiis cumque ipsum quidem culpa totam commodi quasi odio aliquid a accusamus temporibus.<br /></Body1>
-                    </CardPreview>
-                </Card>
+                    <Card appearance='subtle'>
+                        <CardHeader 
+                            image={
+                                <ChatMultipleMinusRegular style={{fontSize: '20px'}} />
+                            }
+                            header={
+                                <Body1Strong>AI received message</Body1Strong>
+                            } />
+                        <CardPreview className='msg-container'>
+                            <Body1>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nisi, ipsam sequi minima optio in vel sed dicta blanditiis cumque ipsum quidem culpa totam commodi quasi odio aliquid a accusamus temporibus.<br /></Body1>
+                        </CardPreview>
+                    </Card>
 
-                <Card>
-                    <CardHeader 
-                        image={
-                            <BotRegular style={{fontSize: '20px'}} />
-                        }
-                        header={
-                            <Body1Strong>Response</Body1Strong>
-                        }
-                    />
-                    <CardPreview className='msg-container'>
-                        <CollapseCard title='Thoughts'>
-                            <Body1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt dolores corrupti neque numquam repellendus distinctio, pariatur unde id tempora natus! Praesentium fugiat totam vitae possimus placeat deleniti! Mollitia, sit id!Lorem Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates explicabo fugiat nobis culpa vel expedita deserunt, aspernatur magnam error at officia, dolor neque. Praesentium quae voluptatum voluptates totam. Velit, rerum.</Body1>
-                        </CollapseCard>
-                        <Body1>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eveniet ea corrupti quibusdam minus voluptatum sapiente ratione nemo assumenda quae? Minima natus qui quae iusto doloribus tempore non nam quia quo!<br /></Body1>
-                        <CollapseCard title='JavaScript'>
-                            <Body1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt dolores corrupti neque numquam repellendus distinctio, pariatur unde id tempora natus! Praesentium fugiat totam vitae possimus placeat deleniti! Mollitia, sit id!</Body1>
-                        </CollapseCard>
-                    </CardPreview>
-                </Card>
+                    <Card appearance='outline'>
+                        <CardHeader 
+                            image={
+                                <BotRegular style={{fontSize: '20px'}} />
+                            }
+                            header={
+                                <Body1Strong>Response</Body1Strong>
+                            }
+                        />
+                        <CardPreview className='msg-container'>
+                            <Text>
+                                <MsgMD>
+                                    {`
+<think>
+
+段落 **粗体**、*斜体* 和 \`console.log()\` [链接](https://github.com)
+
+</think>
+
+# 主标题 H1
+
+## 副标题 H2
+
+114514
+
+1. 列表1
+2. 列表2
+3. 列表3
+
+- 1
+    - 1.1
+    - 1.2
+- 2
+
+\> 引用
+
+\`\`\`javascript:run
+console.log('Hello, world!');
+\`\`\`
+
+\`\`\`json:call
+{"a": "b", "c": 114514}
+\`\`\`
+
+---
+
+                                    `}
+                                </MsgMD>
+                            </Text>
+                        </CardPreview>
+                    </Card>
+
+                </div>
             </div>
         </div>
     );
